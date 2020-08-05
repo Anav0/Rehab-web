e724e683d1bb7a48d6f74acbe0c685bbb3b554f2
Igor Motyka <igor_motyka@mail.com>
2020-08-05 13:06:24 +0200

    New mocked patients and appointments

--- src/mock/appointments.ts
+++ src/mock/appointments.ts
@@ -3,21 +3,35 @@ import patients from "./patients";
 import treatments from "./treatments";
 
 export default [
-
-  new Appointment(new Date("July 28, 2020 07:00"), treatments[0], [
-    patients[0],
-    patients[1],
-  ]),
-  new Appointment(new Date("July 28, 2020 07:20"), treatments[3], [
-    patients[0],
-    patients[1],
-  ]),
   new Appointment(new Date("July 28, 2020 07:00"), treatments[1], [
+    patients[0],
+    patients[1],
     patients[2],
     patients[3],
   ]),
-  new Appointment(new Date("July 28, 2020 09:25"), treatments[2], [
-    patients[2],
-    patients[3],
+    new Appointment(new Date("July 28, 2020 07:00"), treatments[0], [
+    patients[4],
+    patients[5],
+    patients[6],
+    patients[7],
+    patients[16],
+    patients[17],
+  ]),
+
+  new Appointment(new Date("July 28, 2020 07:30"), treatments[2], [
+    patients[8],
+    patients[9],
+  ]),
+  new Appointment(new Date("July 28, 2020 08:00"), treatments[3], [
+    patients[10],
+    patients[11],
+  ]),
+  new Appointment(new Date("July 28, 2020 08:05"), treatments[1], [
+    patients[12],
+    patients[13],
+  ]),
+  new Appointment(new Date("July 28, 2020 12:00"), treatments[3], [
+    patients[14],
+    patients[15],
   ]),
 ];
--- src/mock/patients.ts
+++ src/mock/patients.ts
@@ -1,8 +1,20 @@
 import { Patient, Sex } from "../models/patient";
 
-export default [
-  new Patient("Janina ze Szczecina", Sex.FEMALE),
-  new Patient("Tomek Klocek", Sex.MALE),
-  new Patient("Radek Sitek", Sex.MALE),
-  new Patient("Leszke Śmieszke", Sex.MALE),
-];
+let firstNameM = ['Jacek', 'Tomek', 'Andrzej','Wiesław','Żytomir','Chlebosław','Lesław','Igor','Leszek']
+let firstNameF = ['Ewa', 'Janina', 'Dorota','Wiesława','Żytomira','Chlebosława','Lesława','Hermione','Leszeksława']
+let lastName = ['Kowalski','Nowak','Żyto','Pszenica','Owies','Chmiel','Kukurydza','Oregano','Bazylia']
+let patients: Patient[] = []
+
+for (let i = 0; i < 2000; i++) {
+  let name = ''
+  let sex = i % 4 ? Sex.FEMALE : Sex.MALE
+  if(sex==Sex.MALE)
+    name = firstNameM[Math.floor(Math.random() * firstNameM.length)]
+  else
+    name = firstNameF[Math.floor(Math.random() * firstNameM.length)]
+
+  name+=` ${lastName[Math.floor(Math.random() * firstNameM.length)]}`
+  patients.push(new Patient(name,sex))
+}
+
+export default patients;
