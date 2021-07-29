60b3bc6b86d9d2450279957ee1d9b943d97afbf8
Igor Motyka <igor_motyka@mail.com>
2021-07-29 20:11:03 +0200

    Added action btns to result screen
    tmp

--- src/components/pages/ResultsPage.svelte
+++ src/components/pages/ResultsPage.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
-  import {
-    DataTable,
-    Button,
-  } from "carbon-components-svelte";
+  import { DataTable, Loading, ToastNotification, Button } from "carbon-components-svelte";
   import type { DataTableHeader } from "carbon-components-svelte/types/DataTable/DataTable";
   import type { Term } from "../../models/term";
   import { proposition } from "../../stores/scheduling";
+  import TrashCan16 from "carbon-icons-svelte/lib/TrashCan16";
+  import Reset16 from "carbon-icons-svelte/lib/Reset16";
+  import { displayOnMain } from "../../stores/mainPanel";
+  import { schedulingRequest } from "../../stores/scheduling";
+  import { api } from "../../api";
 
   let headers: DataTableHeader[] = [
     {
       key: "StartDate",
-      value: "Proponowany termin",
-      display: (date) => new Date(date).toLocaleString(),
+      value: "Data",
+      display: (date) => new Date(date).toLocaleDateString(),
       sort: (a, b) => (new Date(a) < new Date(b) ? -1 : 1),
+    },
+    {
+      key: "hours",
+      value: "Godziny",
     },
     {
       key: "PlaceName",
@@ -26,19 +32,53 @@
 
   proposition.subscribe((value) => {
     if (!value) return;
+    rows = [];
     for (let i = 0; i < value.ProposedTrms.length; i++) {
       const terms: Term[] = value.ProposedTrms[i];
-      for (let j = 0; j < terms.length; j++) {
-        const term = terms[j];
-        term["id"] = term.Id;
-        rows.push(term);
-      }
+      const term = terms[0];
+      term["id"] = term.Id;
+      term["hours"] = `${new Date(terms[0].StartDate).toLocaleTimeString()} - ${new Date(
+        terms[terms.length - 1].EndDate
+      ).toLocaleTimeString()}`;
+
+      rows.push(term);
     }
     rows = [...rows];
   });
+  let isLoading = false;
+  let errMsg = "";
+  let askForProposition = async () => {
+    isLoading = true;
+    try {
+      const { data: result } = await api.scheduling.proposition($schedulingRequest);
+      $proposition = result;
+    } catch (err) {
+      if (err.response) {
+        errMsg = err.response.data.error;
+        console.error(err.response);
+      } else {
+        errMsg = "Wystąpił błąd przy wyznaczaniu";
+      }
+    } finally {
+      isLoading = false;
+    }
+  };
 </script>
 
 <div class="result page">
+  {#if isLoading}
+    <Loading description="Trwa wyznaczanie terminów..." />
+  {/if}
+  {#if errMsg != ""}
+    <ToastNotification
+      lowContrast
+      style="position: absolute; top: 1rem; right: 4rem;"
+      on:close={() => (errMsg = "")}
+      timeout={5000}
+      title="Błąd przy wyznaczaniu"
+      subtitle={errMsg}
+    />
+  {/if}
   <DataTable
     title="Proponowane terminy dla zlecenia"
     description="Terminy proponowane przez program mogą zostać ręcznie dostosowane i autmatycznie sprawdzone"
@@ -49,11 +89,24 @@
   >
     <span slot="cell" let:cell let:row>
       {#if cell.key === "action"}
-        <Button kind="tertiary">Dostosuj</Button>
+        <Button kind="ghost">Dostosuj</Button>
       {:else if cell.key == "StartDate"}{cell.display(cell.value)}
       {:else}{cell.value}
       {/if}
     </span>
+    <div class="result-toolbar">
+      <Button
+        on:click={() => {
+          $displayOnMain = "referral";
+          $proposition = null;
+        }}
+        kind="danger"
+        iconDescription="Przerwij wyznaczanie"
+        icon={TrashCan16}
+      />
+      <Button kind="ghost" on:click={askForProposition} iconDescription="Wyznacz ponownie" icon={Reset16} />
+      <Button kind="primary">Akceptuj</Button>
+    </div>
   </DataTable>
 </div>
 
@@ -62,5 +115,10 @@
     width: 100%;
     height: 100%;
     overflow: auto;
+  }
+  .result-toolbar {
+    display: flex;
+    align-items: flex-end;
+    justify-content: end;
   }
 </style>
