d972de08eb3b126574d1dbca1ed637f71c83bc8a
Igor Motyka <igor_motyka@mail.com>
2021-08-29 09:53:12 +0200

    Fixed referral store date

--- src/stores/referral.ts
+++ src/stores/referral.ts
@@ -2,7 +2,7 @@ import { writable } from "svelte/store";
 import type { ReferralsRangePayload } from "@/api/payload-models";
 import type { Referral } from "@/models/referral";
 
-let startDate = new Date(2018, 9, 1, 6, 0, 0);
+let startDate = new Date(2018, 9, 1, 8, 0, 0);
 let endDate = new Date(2018, 9, 2, 18, 0, 0);
 
 export const referralsRangePayload = writable<ReferralsRangePayload>({
